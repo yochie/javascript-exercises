537ec5fa5506dfcc32339007c2f92ba848402a26
yoann <yoann.ochietti@gmail.com>
2024-10-04 12:41:58 -0400

    Add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
