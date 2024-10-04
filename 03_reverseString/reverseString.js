be732efc7a10420724fc0b67aa2361e4d969d7ec
yoann <yoann.ochietti@gmail.com>
2024-10-04 12:07:15 -0400

    Add string reversing function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let chars = string.split("").reverse();
+    return chars.join("");
 };
 
 // Do not edit below this line
