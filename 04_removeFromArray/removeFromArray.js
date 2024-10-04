0542f4aa6083b24dfc3dfa2598d6dd7d4a564b50
yoann <yoann.ochietti@gmail.com>
2024-10-04 12:37:08 -0400

    Filter list to remove items

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removelist) {
+    let filtered = array.filter(
+        (element) => 
+        {return removelist.findIndex((toRemove) => {return toRemove === element}) == -1;}
+    );
+    return filtered;
 };
 
 // Do not edit below this line
